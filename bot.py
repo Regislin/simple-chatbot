488f0bfeb478b7750290a5593e54db7c62b91b8b
Regislin <137523454+Regislin@users.noreply.github.com>
2025-07-09 20:05:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,6 +46,8 @@ def chat():
         response = "Goodbye! Have a great day! 👋"
 
     return jsonify({'response': response})
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
